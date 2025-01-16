3f6f8b095ce81a540250e092e12e38fee88df754
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2025-01-16 03:48:56 +0300

    Create _version.py

---
+++ waveguide/_version.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+__version__ = "0.0.3"
+__license__ = "MIT"
+__description__ = "waveguide"
+__author__ = "Mehmet Keçeci"
+__url__ = "https://github.com/garrettj403/Waveguide/"
+__dependencies__ = ["python>=3.8"]
