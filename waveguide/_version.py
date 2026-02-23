038d812cf4e530574d70b43260d875f8f4331606
Mehmet Keçeci <WhiteSymmetry@users.noreply.github.com>
2026-02-23 07:57:36 +0000

    Update _version.py

--- waveguide/_version.py
+++ waveguide/_version.py
@@ -3,6 +3,6 @@ from __future__ import annotations
 __version__ = "0.0.3"
 __license__ = "MIT"
 __description__ = "waveguide"
-__author__ = "Mehmet Keçeci"
+__author__ = "John Garrett"
 __url__ = "https://github.com/garrettj403/Waveguide/"
 __dependencies__ = ["python>=3.8"]
